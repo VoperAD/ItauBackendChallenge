3a243d3d29649b3f9bbfb289e3fcaedfa4493bba
VoperAD <victoraguiarrodrigues@yahoo.com>
2025-02-01 05:31:29 +0100

    feat: initial commit

---
+++ src/test/java/dev/voper/itauchallenge/ItauChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.voper.itauchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
